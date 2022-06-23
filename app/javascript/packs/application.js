af793baa15aaf3700d931ad09f357b2640533ef1
xion0207 <xionm2727@gmail.com>
2022-06-23 12:20:14 +0000

    [Fix] Skippr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'jquery'
-import "skippr.min"
+// import "skippr.min"
 import "script.js"
 import "popper.js";
 import "bootstrap";
